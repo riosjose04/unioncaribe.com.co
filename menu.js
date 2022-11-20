3983aa1899b1797a8ddabab46deedb2c6387efca
Jose Ríos <42424912+riosjose04@users.noreply.github.com>
2022-11-19 19:11:49 -0500

    Fixid movil menu

--- menu.js
+++ menu.js
@@ -1,14 +1,10 @@
 function openMenu(){
-    document.getElementById('close-menu').classList.remove('hidden');
-    document.getElementById('close-menu').classList.add('show');
-    document.getElementById('open-menu').classList.remove('show');
-    document.getElementById('open-menu').classList.add('hidden');
     document.getElementById('hidden-nav').id = 'center-nav'
+    document.getElementById('open-menu').id = 'opened-menu'
+    document.getElementById('closed-menu').id = 'close-menu'
 }
 function closeMenu(){
-    document.getElementById('open-menu').classList.remove('hidden');
-    document.getElementById('open-menu').classList.add('show');
-    document.getElementById('close-menu').classList.remove('show');
-    document.getElementById('close-menu').classList.add('hidden');
     document.getElementById('center-nav').id = 'hidden-nav'
+    document.getElementById('opened-menu').id = 'open-menu'
+    document.getElementById('close-menu').id = 'closed-menu'
 }
